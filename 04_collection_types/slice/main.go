9ace0bafd705e4ab9922bf4a605062c1c85f42f8
roliiii <roli4759@hotmail.com>
2019-02-18 12:16:02 -0800

    slice

---
+++ 04_collection_types/slice/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var x []string
+	x := make([]string, 3, 5) //length, capacity
+	x[0] = "H"
+	x[1] = "el"
+	x[2] = "lo"
+
+	fmt.Println(x[:1])
+	fmt.Println(x[1:])
+	fmt.Printf("len: %d\n", len(x)) //3
+	fmt.Printf("cap: %d\n", cap(x)) //5
+
+	y := append(x, "world")
+	fmt.Println(y)
+	fmt.Printf("len: %d\n", len(y)) //4
+	fmt.Printf("cap: %d\n", cap(y)) //5
+
+	z := make([]string, 1)
+	copy(z, y)
+	fmt.Println(z)
+	fmt.Printf("len: %d\n", len(z)) //1
+	fmt.Printf("cap: %d\n", cap(z)) //1
+}
